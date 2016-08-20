fa3d0425773538eae058aca39f2f65fd0285eb1c
Adit Deshpande <adeshpande3@g.ucla.edu>
2016-08-20 07:31:47 -0400

    Add files via upload

--- KaggleMNIST.py
+++ KaggleMNIST.py
@@ -24,22 +24,22 @@ for row in csv_file:
 	temp = np.ones((1,784))
 	for num in range(1,784):
 		temp[0,num - 1] = row[num]
-	temp = np.reshape((28,28))
-	xTrain[counter] = temp
+	temp = np.reshape(temp, (28,28))
+	xTrain[counter,:,:] = temp
 	counter = counter + 1
 print yTrain
 
 
-model = Sequential()
-model.add(Convolution2D(32, 3, 3, border_mode='valid', input_shape=(img_channels, img_rows, img_cols)))
-model.add(Activation('relu'))
-model.add(Dropout(0.05))
+#model = Sequential()
+#model.add(Convolution2D(32, 3, 3, border_mode='valid', input_shape=(img_channels, img_rows, img_cols)))
+#model.add(Activation('relu'))
+#model.add(Dropout(0.05))
 
-model.add(Convolution2D(64, 3, 3, border_mode='valid'))
-model.add(Activation('relu'))
-model.add(Dropout(0.10))
+#model.add(Convolution2D(64, 3, 3, border_mode='valid'))
+#model.add(Activation('relu'))
+#model.add(Dropout(0.10))
 
-sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd)
+#sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+#model.compile(loss='categorical_crossentropy', optimizer=sgd)
 
-model.fit(X_train, Y_train, batch_size=32, nb_epoch=1)
+#model.fit(X_train, Y_train, batch_size=32, nb_epoch=1)
