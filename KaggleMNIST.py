52f5e84d549aa4cc74a0e48b782daae93f6ab165
Adit Deshpande <adeshpande3@g.ucla.edu>
2016-08-27 10:11:38 -0400

    Add files via upload

--- KaggleMNIST.py
+++ KaggleMNIST.py
@@ -1,3 +1,18 @@
+#############
+
+# This program is a simple digit classifier using the MNIST dataset. 
+# I will be using the Keras neural network library with a Theano 
+# backend. I will be submitting this to the competition on Kaggle.
+# This competition has 42,000 training images and 28,000 test
+# images. At the end of the program, a text file (csv format) named results
+# will be created. It will have ImageId in one column (numbers from
+# 1 to 28,000), and the prediction labels for each test image
+# in another column. 
+
+#############
+
+# Imports of the different Keras layers and optimizers, as well as
+# importing numpy and csv
 from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
@@ -6,16 +21,23 @@ from keras.utils import np_utils
 import csv
 import numpy as np
 
-# input image dimensions
+# VARIABLES
 img_rows, img_cols = 28, 28
-# the MNIST images are black and white
 img_channels = 1
+num_pixels = img_cols*img_rows
 num_classes = 10
-yTrain = np.ones((42000))
-xTrain = np.ones((42000,img_channels,img_cols,img_rows))
+num_trainImages = 42000
+num_testImages = 28000
+
+# CREATE TRAINING DATASET
+yTrain = np.ones((num_trainImages))
+xTrain = np.ones((num_trainImages,img_channels,img_cols,img_rows))
 counter = 0
+# We want to skip the first row in the csv file because it just 
+# contains column header
 skip = True
 
+# train.csv should be in the same folder as this file
 trainFile = open('train.csv')
 csv_file = csv.reader(trainFile)
 for row in csv_file:
@@ -23,48 +45,45 @@ for row in csv_file:
 		skip = False
 		continue
 	yTrain[counter] = row[0]
-	temp = np.ones((1,784))
-	for num in range(1,784):
+	temp = np.ones((1,num_pixels))
+	for num in range(1,num_pixels):
 		temp[0,num - 1] = row[num]
 	temp = np.reshape(temp, (img_rows,img_cols))
 	xTrain[counter,0,:,:] = temp
 	counter = counter + 1
 
-testFile = open('test.csv')
-csv_file2 = csv.reader(testFile)
-yTest = np.ones((28000))
-xTest = np.ones((28000,img_channels,img_cols,img_rows))
+# CREATE TEST DATASET
+yTest = np.ones((num_testImages))
+xTest = np.ones((num_testImages,img_channels,img_cols,img_rows))
 skip2 = True
 counter2 = 0
+
+testFile = open('test.csv')
+csv_file2 = csv.reader(testFile)
 for row in csv_file2:
 	if (skip2 == True):
 		skip2 = False
 		continue
 	yTest[counter2] = row[0]
-	temp = np.ones((1,784))
-	for num in range(1,784):
+	temp = np.ones((1,num_pixels))
+	for num in range(1,num_pixels):
 		temp[0,num - 1] = row[num]
 	temp = np.reshape(temp, (img_rows,img_cols))
 	xTest[counter2,0,:,:] = temp
 	counter2 = counter2 + 1
 
-# convert class vectors to binary class matrices
+# Convert class vectors to binary class matrices
 yTrain = np_utils.to_categorical(yTrain, num_classes)
 yTest = np_utils.to_categorical(yTest, num_classes)
 
-#Network Architecture
+# NETWORK ARCHITECTURE
 model = Sequential()
 model.add(Convolution2D(32, 3, 3, border_mode='valid', input_shape=(img_channels,img_rows, img_cols)))
-
 model.add(Activation('relu'))
 model.add(Convolution2D(32, 3, 3, border_mode='valid'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Convolution2D(192, 3, 3, border_mode='valid'))
 model.add(Dropout(0.10))
-#model.add(Activation('relu'))
-#model.add(Convolution2D(256, 3, 3, border_mode='valid'))
-#model.add(Activation('relu'))
 model.add(Flatten())
 model.add(Dense(128))
 model.add(Activation('relu'))
@@ -72,18 +91,18 @@ model.add(Dropout(0.25))
 model.add(Dense(num_classes))
 model.add(Activation('softmax'))
 
-#Optimizers and Testing
+# TRAINING
 model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
 model.fit(xTrain, yTrain, batch_size=32, nb_epoch=8,validation_data=(xTest, yTest),shuffle=True)
 
 #Saving predictions into a test file that can be uploaded to Kaggle
 #NOTE: You have to add a header row before submitting the txt file
-results = np.zeros((28000,2))
-for num in range(1,28001):	
+results = np.zeros((num_testImages,2))
+for num in range(1,num_testImages + 1):	
 	results[num - 1,0] = num
+
+# TESTING
 temp = model.predict_classes( xTest, batch_size=32, verbose=1)
-print temp.shape
-#temp is a 
-for num in range(0,28000):	
+for num in range(0,num_testImages):	
 	results[num,1] = temp[num]
-np.savetxt('result.txt', results, delimiter=',', fmt = '%i')  
+np.savetxt('result.csv', results, delimiter=',', fmt = '%i')  
